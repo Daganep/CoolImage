b86dd85ab9004b8114752895e8979bb3792a8dd7
Oleg <49286750+Daganep@users.noreply.github.com>
2020-09-24 13:01:37 +0300

    Inint Retrofit + Gson

--- CoolImage/app/src/main/java/com/geekbrains/coolimage/model/retrofit/RetrofitApi.java
+++ CoolImage/app/src/main/java/com/geekbrains/coolimage/model/retrofit/RetrofitApi.java
@@ -1,4 +1,31 @@
 package com.geekbrains.coolimage.model.retrofit;
 
+import com.geekbrains.coolimage.model.entity.PixabayResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitApi {
+    public Observable<PixabayResponse> requestServer(){
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+
+        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create(gson);
+
+        RetrofitService api = new Retrofit.Builder()
+                .baseUrl("https://pixabay.com/api/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(gsonConverterFactory)
+                .build()
+                .create(RetrofitService.class);
+
+        return api.getPictures().subscribeOn(Schedulers.io());
+    }
 }
--- CoolImage/app/src/main/java/com/geekbrains/coolimage/model/retrofit/RetrofitService.java
+++ CoolImage/app/src/main/java/com/geekbrains/coolimage/model/retrofit/RetrofitService.java
@@ -1,4 +1,11 @@
 package com.geekbrains.coolimage.model.retrofit;
 
-public class RetrofitService {
+import com.geekbrains.coolimage.model.entity.PixabayResponse;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+public interface RetrofitService {
+    @GET("?key=17902344-d6abfe0f0033b2d53a27c83f0&image_type=photo")
+    Observable<PixabayResponse> getPictures();
 }
