16152751737e8b34b6f0d648b8a35a942879499c
Oleg <49286750+Daganep@users.noreply.github.com>
2020-09-24 12:52:01 +0300

    Init RecyclerView and layputs for Recycler

--- CoolImage/app/src/main/java/com/geekbrains/coolimage/view/main/MainActivity.java
+++ CoolImage/app/src/main/java/com/geekbrains/coolimage/view/main/MainActivity.java
@@ -1,16 +1,33 @@
 package com.geekbrains.coolimage.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.geekbrains.coolimage.R;
+import com.geekbrains.coolimage.view.recyclerview.RecyclerViewAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void init(){
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerViewAdapter = new RecyclerViewAdapter();
     }
 }
--- CoolImage/app/src/main/java/com/geekbrains/coolimage/view/recyclerview/RecyclerViewAdapter.java
+++ CoolImage/app/src/main/java/com/geekbrains/coolimage/view/recyclerview/RecyclerViewAdapter.java
@@ -1,4 +1,48 @@
 package com.geekbrains.coolimage.view.recyclerview;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.geekbrains.coolimage.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ImageViewHolder> {
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutId = R.layout.image_view_holder;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutId, parent, false);
+
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ImageViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        void bind(int position){
+
+        }
+    }
 }
+
