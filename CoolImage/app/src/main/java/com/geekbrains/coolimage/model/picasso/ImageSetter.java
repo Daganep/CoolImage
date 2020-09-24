359e7910d7d3df8972848fc38ddf44e4585b9ceb
Oleg <49286750+Daganep@users.noreply.github.com>
2020-09-24 15:01:16 +0300

    Init Picasso in ImageSetter

--- CoolImage/app/src/main/java/com/geekbrains/coolimage/model/picasso/ImageSetter.java
+++ CoolImage/app/src/main/java/com/geekbrains/coolimage/model/picasso/ImageSetter.java
@@ -1,4 +1,15 @@
 package com.geekbrains.coolimage.model.picasso;
 
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 public class ImageSetter {
+
+    public void setImage(String url, ImageView imageView){
+        Picasso
+                .get()
+                .load(url)
+                .into(imageView);
+    }
 }
